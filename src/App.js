6f5d65b48ad1f080edaabcfd1b266152359efeab
isaias-cloud <isaias-santiago-toribio@outlook.com>
2021-06-04 09:43:30 -0400

    Header structure and css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './Components/Header/Header';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <Fragment>
+            <Header />
+        </Fragment>
     );
 }
 
